36bb5d53c73c2e3f02776fa691d96da14f50be5a
JAYDISH KENNEDY J <jaydishkennedy@gmail.com>
2025-12-28 21:58:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import pandas as pd
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-
-
-# --------------------------------------------------
-# Page Configuration (ONLY ONCE)
-# --------------------------------------------------
-import streamlit as st
+import json
 
 # --------------------------------------------------
 # Page Configuration
