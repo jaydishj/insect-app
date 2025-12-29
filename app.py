49c3bd22badc474aa5eb43ec82bbeacf49f473fe
JAYDISH KENNEDY J <jaydishkennedy@gmail.com>
2025-12-30 00:22:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -401,9 +401,9 @@ def classification_page():
         else:
             st.warning("Detailed information for this species is not available in the database.")
 
-        if st.button("⬅️ Back to Home"):
+          if st.button("⬅️ Back to Home"):
             st.session_state.page = "intro"   
-      else:
+        else:
           st.info("Please upload an image to begin identification.")
 
 # --------------------------------------------------
